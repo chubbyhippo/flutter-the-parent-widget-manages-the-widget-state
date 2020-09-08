a8f68404c894846d2f238e005d6da986c5bb236a
ch <mkmsacc@outlook.com>
2020-09-08 12:38:41 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/parent_widget_manages_state/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.parent_widget_manages_state
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+class TapboxB extends StatelessWidget {
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  const TapboxB({Key key, this.active, this.onChanged}) : super(key: key);
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+
+    );
+  }
+}
+
+
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+
+
+void main() {
+  runApp(MaterialApp(
+    title: "Parent widget manages the widget's state",
+    home: ParentWidget(),
+  ));
+}
+
